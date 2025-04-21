321eaee395647b3f7b7c3adf2af51883bcaa220d
furkandur <furkandursun3428@gmail.com>
2025-04-21 17:32:32 +0300

    add mongodb setup for dev

---
+++ server/mongo/mongo-init.js
@@ -0,0 +1,14 @@
+db.createUser({
+  user: "the_username",
+  pwd: "the_password",
+  roles: [
+    {
+      role: 'dbOwner',
+      db: 'the_database'
+    }
+  ]
+})
+
+db.createCollection('users');
+
+db.users.insert({ name: 'Cerosh' });
